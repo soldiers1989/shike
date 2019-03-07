4c8082074184ac4b56803b86d74c964645a645c6
fankaidi <fankaidid@163.com>
2019-03-07 22:05:58 +0800

    更新版本

--- src/main/java/com/kensure/shike/constant/BusiConstant.java
+++ src/main/java/com/kensure/shike/constant/BusiConstant.java
@@ -8,7 +8,7 @@ package com.kensure.shike.constant;
  */
 public class BusiConstant {
 
-	public static final String version = "1.1";
+	public static final String version = "1.3";
 	
 	public static final String context = "/shike";
 
